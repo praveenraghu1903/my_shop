ca5e5a9222b4f0677ef1c0168e803d921780770c
praveenraghu1903 <swift3homes@gmail.com>
2026-01-02 20:10:02 +0530

    Seed test product and non-interactive superuser in build

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python setup_initial_data.py
+python setup_test_product.py
+
+# Create superuser non-interactively if env vars are set on Render
+# DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_PASSWORD, DJANGO_SUPERUSER_EMAIL
+python manage.py createsuperuser --noinput || true
